0e60d3f5b9ff5ae0465beae79314cbceb45ef5b0
Laurent Michel <laurent.michel@cognex.com>
2023-02-09 08:36:11 +0100

    fix url of helm registry

--- .teamcity/integration/Integration.kts
+++ .teamcity/integration/Integration.kts
@@ -44,7 +44,7 @@ class Integration(
                 set -x
                 helm dependency update hasura
                 CHART_PACKAGE=${'$'}(helm package --app-version %hasura.version% --version %hasura.chart.version% hasura | cut -d":" -f2 | tr -d '[:space:]')
-                curl -is -u "%system.package-manager.deployer.username%:%system.package-manager.deployer.password%" "https://%system.package-manager.hostname%" --upload-file "${'$'}CHART_PACKAGE"
+                curl -is -u "%system.package-manager.deployer.username%:%system.package-manager.deployer.password%" "https://%system.package-manager.hostname%/repository/helm-hosted/" --upload-file "${'$'}CHART_PACKAGE"
             """.trimIndent()
             workingDir = "./softozor"
             dockerPull = true
